977267dedd0caffbd0d47336b431a92a7f7a964d
bethbenjamin <bethbenjamin@gmail.com>
2018-01-29 11:41:27 -0500

    cat is hungry method. First fill of info

--- IntroToOOPSpring18/Cat.cs
+++ IntroToOOPSpring18/Cat.cs
@@ -8,5 +8,44 @@ namespace IntroToOOPSpring18
 {
     class Cat
     {
+        //I need STATES
+
+        //fields
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+            public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //I NEED BEHAVIORS
+
+        //Constructors
+        public Cat()
+        {
+            //default constructor
+            //takes no parameters (nothing in the parentheses)
+        }
+        public Cat (string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+        //method
+        public void Eat()
+        {
+            if (isHungry)
+            {
+                isHungry = false;
+            }
+            Console.WriteLine("is the cat hungry?" + isHungry);
+        }
+
     }
 }
